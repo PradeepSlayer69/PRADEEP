52a40cf7d5c0cd111cdb0cd57a17138e4519a92d
pradeep <113674148+PradeepSlayer69@users.noreply.github.com>
2022-10-14 15:41:37 +0530

    Update f.c

--- f.c
+++ f.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 int main()
 {
+    int a=10;
+    int b=2;
+    if(a>b)
+    {
     printf("this is DevOps lab");
+    }
+    else
+    {
+        printf("this is not opening");
+    }
 
 
     return 0;
